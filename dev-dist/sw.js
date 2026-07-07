5cd0071d03cda8dd372fd9b6add8eef63646080b
tucno21 <carlitostucno@gmail.com>
2026-07-06 20:15:39 -0500

    eliminar tipos de base de datos

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-aeb6ecaf'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.ole8icr4auo"
+    "revision": "0.sau5a01va08"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
